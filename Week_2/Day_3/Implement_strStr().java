582c9edcd8210b313dc6ecb3e0d23766d55bcc33
Sonam-yadav05 <sonamyadav9351@gmail.com>
2026-06-10 19:10:26 +0530

    Add files via upload

---
+++ Week_2/Day_3/Implement_strStr().java
@@ -0,0 +1,7 @@
+class Solution {
+    public int strStr(String nums, String tar) {
+
+        
+        return nums.indexOf(tar);
+    }
+}
---
+++ Week_2/Day_3/Is_Subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int j = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            boolean found = false;
+            while (j < t.length()) {
+                if (t.charAt(j) == ch) {
+                    found = true;
+                    j++;
+                    break;
+                }
+                j++;
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
