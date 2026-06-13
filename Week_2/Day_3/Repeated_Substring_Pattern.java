7af3831cf84a72ec7ab4e2f9b1751a1c8b509386
Sonam-yadav05 <sonamyadav9351@gmail.com>
2026-06-13 09:05:57 +0530

    Add files via upload

---
+++ Week_2/Day_3/Repeated_Substring_Pattern.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public boolean repeatedSubstringPattern(String s) {
+
+        int n = s.length();
+
+        for (int len = 1; len <= n / 2; len++) {
+
+            if (n % len != 0) {
+                continue;
+            }
+
+            String pattern = s.substring(0, len);
+            StringBuilder formed = new StringBuilder();
+
+            int times = n / len;
+
+            for (int i = 0; i < times; i++) {
+                formed.append(pattern);
+            }
+
+            if (formed.toString().equals(s)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
